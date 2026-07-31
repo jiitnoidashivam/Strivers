c147982085b45feab09ffdb8e46d51f254208994
Shivam Panwar <shivampanwar030@gmail.com>
2026-07-31 18:13:07 +0530

    Missing Element Interview method(XOR)

--- 4 Array Problems/6 missing number.cpp
+++ 4 Array Problems/6 missing number.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 using namespace std;
 
+//the only problem with sum method is that if number is very large, n(n+1)/2 can overflow the max value of int
+//this is the most common method for interviews
+
 int missing(int arr[],int n){
-    int sum1=0;
-    int sum2=(n*(n+1))/2;
-    for(int i=0;i<n;i++){
-        sum1+=arr[i];
-    }
-    if(sum1!=sum2){
-    return (sum2-sum1);
-    }
-    else{
-    return n+1;
-    }
+    int ans=0;
+for(int i=0;i<=n;i++){          //mind the equal sign here , because 1 elemnt will obviously be missing , so this is here(even if it it the next elemnt)
+ans=ans^i;
+}
+for(int i=0;i<n;i++){
+    ans=ans^arr[i];
+}
+return ans;
 }
 
 int main() {
-    int arr[]={0,2,3,4,5};
-    int x=missing(arr,5);
+    int arr[]={0,1,2,3,4,5};
+    int x=missing(arr,6);
     cout<<x<<endl;
     return 0;
 }
